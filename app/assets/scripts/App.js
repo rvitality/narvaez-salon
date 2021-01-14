6ed778895d5a914853c03a8d865f7468aaf8fa96
Renz Vital <vital.renzjohn@gmail.com>
2021-01-14 05:49:03 -0800

    Implemented hot reloading.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import "../styles/styles.css";
 
 console.log("hello dog...");
+
+if (module.hot) {
+    module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,18 @@ module.exports = {
         filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
+    devServer: {
+        // in charge of saving changes made on the html, full reload
+        before: function (app, server) {
+            server._watch("./app/**/*.html");
+        },
+        contentBase: path.join(__dirname, "app"),
+        // not a full reload, css and js
+        hot: true,
+        host: "0.0.0.0",
+        port: 3000,
+    },
     mode: "development",
-    watch: true,
     module: {
         rules: [{
             test: /\.css$/i,

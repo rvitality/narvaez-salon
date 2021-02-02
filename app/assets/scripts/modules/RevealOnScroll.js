b722ad44a5437cf1b4546b09cad3cf42bbc2cbb9
Renz Vital <vital.renzjohn@gmail.com>
2021-02-02 11:56:00 +0800

    Finished the reveal on scroll functionality.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,15 +1,20 @@
 import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
 
 class RevealOnScroll {
     constructor() {
         this.revealItems = document.querySelectorAll(".reveal-item");
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+        this.browserHeight = window.innerHeight;
         this.areHidden();
         this.events();
     }
 
     events() {
         window.addEventListener("scroll", this.scrollThrottle);
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     calcCaller() {
@@ -21,15 +26,17 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el) {
-        let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
-        if (scrollPercent < 75) {
-            el.classList.add("reveal-item--is-visible");
-            el.isRevealed = true;
+        if (window.scrollY + this.browserHeight > el.offsetTop) {
+            console.log("cacl...");
+            let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
+            if (scrollPercent < 75) {
+                el.classList.add("reveal-item--is-visible");
+                el.isRevealed = true;
 
-            if (el.isLastItem) {
-                window.removeEventListener("scroll", this.scrollThrottle);
+                if (el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle);
+                }
             }
-
         }
     }
 

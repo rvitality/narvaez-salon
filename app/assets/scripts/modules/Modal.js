f2898541b3026cbe4d8313ef01707f5c5bd70e74
Renz Vital <vital.renzjohn@gmail.com>
2021-02-03 16:37:48 +0800

    Finished the modal.

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -36,9 +36,16 @@ class Modal {
         document.body.insertAdjacentHTML("beforeend", `
             <div class="modal">
                 <div class="wrapper">
-                    <h1>Hello World!</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ipsum quae autem dicta similique earum.</p>
+                    <a href="#" class="modal__envelope"><i class="fas fa-envelope"></i></a>
+                    <h1 class="modal__main-title">Get in <span class="bold">Touch</span></h1>
+                    <span class="border-bottom"></span>
+                    <p class="modal__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ipsum quae autem dicta similique earum.</p>
                     <span class="modal__close-btn">X</span>
+                    <div class="modal__social-icons">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
                 </div>
             </div>
         `);

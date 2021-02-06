d11fb8a9b9b72b9e8c9b9816a20c01a75f348a03
Renz Vital <vital.renzjohn@gmail.com>
2021-02-06 14:56:18 +0800

    Finished the hamburger menu icon functionality.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,7 @@
 class MobileMenu {
     constructor() {
         this.menuIconBtn = document.querySelector(".menu-icon");
+        this.lowerNav = document.querySelector(".lower-nav");
         this.navbarList = document.querySelector(".primary-nav__nav-links");
         this.headerSection = document.querySelector(".header-section");
         this.events();
@@ -12,6 +13,9 @@ class MobileMenu {
 
     toggleTheMenu() {
         this.navbarList.classList.toggle("primary-nav__nav-links--is-visible");
+        this.lowerNav.classList.toggle("lower-nav--is-flex-col");
+        this.headerSection.classList.toggle("header-section--is-expanded");
+        this.menuIconBtn.classList.toggle("menu-icon__close-btn");
     }
 
 

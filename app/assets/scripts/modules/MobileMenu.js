50c2f79a70746b550f8fddd5f4886d02efbb1ab0
Renz Vital <vital.renzjohn@gmail.com>
2021-02-07 05:15:21 -0800

    Made some changes on the elements to make them responsive.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,8 +1,8 @@
 class MobileMenu {
     constructor() {
         this.menuIconBtn = document.querySelector(".menu-icon");
+        this.primaryNav = document.querySelector(".primary-nav");
         this.lowerNav = document.querySelector(".lower-nav");
-        this.navbarList = document.querySelector(".primary-nav__nav-links");
         this.headerSection = document.querySelector(".header-section");
         this.logoTextElement = document.querySelector(".lower-nav__main-title");
         this.logoImage = document.querySelector(".lower-nav__logo");
@@ -15,13 +15,16 @@ class MobileMenu {
     }
 
     toggleTheMenu() {
-        this.navbarList.classList.toggle("primary-nav__nav-links--is-visible");
+
+        this.primaryNav.classList.toggle("primary-nav--is-visible");
         this.lowerNav.classList.toggle("lower-nav--is-flex-col");
         this.headerSection.classList.toggle("header-section--is-expanded");
         this.menuIconBtn.classList.toggle("menu-icon__close-btn");
         this.logoTextElement.classList.toggle("lower-nav__main-title--text-center");
         this.logoImage.classList.toggle("lower-nav__logo--margin-bottom");
+
         this.logoContainer.classList.toggle("lower-nav__logo-link--display-flex-col");
+        this.logoContainer.classList.toggle("lower-nav__logo-link--remove-max-width");
     }
 
 

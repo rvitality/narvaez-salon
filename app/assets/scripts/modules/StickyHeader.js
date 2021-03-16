d00cf1673c33247232afbffa1b4dcbba515a0ac5
Renz <vital.renzjohn@gmail.com>
2021-03-16 21:25:47 +0800

    Replaced scroll listeners with IntersectionObserver in the navbar

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,90 +1,59 @@
-import throttle from "lodash/throttle";
-import debounce from "lodash/debounce";
-import { times } from "lodash";
-
 class StickyHeader {
     constructor() {
         this.headerSection = document.querySelector(".header-section");
-        this.backToTopBtn = document.querySelector(".back-to-top");
+        this.landingSection = document.querySelector(".landing-section");
         this.pageSections = document.querySelectorAll(".page-section");
-        this.browserHeight = window.innerHeight;
-        this.previousScrollY = window.scrollY;
+        this.navLinks = document.querySelectorAll(".nav__link");
         this.events();
     }
 
     events() {
-        window.addEventListener(
-            "scroll",
-            throttle(() => this.runOnScroll(), 200)
-        );
-        window.addEventListener(
-            "resize",
-            debounce(() => (this.browserHeight = window.innerHeight), 333)
-        );
+        this.runObserver();
+        this.observeSections();
+    }
 
-        this.backToTopBtn.addEventListener("click", () => {
-            window.scrollTo({ left: 0, top: 0 });
+    runObserver() {
+        const stickyNav = (entries) => {
+            const [entry] = entries;
+
+            if (!entry.isIntersecting) {
+                this.headerSection.classList.toggle("header-section--dark");
+            } else {
+                this.headerSection.classList.remove("header-section--dark");
+            }
+        };
+
+        const oberver = new IntersectionObserver(stickyNav, {
+            root: null,
+            threshold: 0.9,
         });
+        oberver.observe(this.landingSection);
     }
 
-    runOnScroll() {
-        this.determineScrollDirection();
+    observeSections() {
+        const highlightLink = (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    // find nav link
+                    const link = document.getElementById(
+                        `${entry.target.id}-link`
+                    );
 
-        if (
-            window.scrollY > 60 &&
-            !this.headerSection.classList.contains(
-                "header-section--is-expanded"
-            )
-        ) {
-            this.headerSection.classList.add("header-section--dark");
-        } else {
-            this.headerSection.classList.remove("header-section--dark");
-        }
-
-        // back to top arrow
-        if (window.scrollY > 600) {
-            this.backToTopBtn.classList.add("back-to-top--show");
-        } else {
-            this.backToTopBtn.classList.remove("back-to-top--show");
-        }
-
-        this.pageSections.forEach((el) => this.calcSection(el));
-    }
-
-    determineScrollDirection() {
-        if (window.scrollY > this.previousScrollY) {
-            this.scrollDirection = "down";
-        } else {
-            this.scrollDirection = "up";
-        }
-
-        this.previousScrollY = window.scrollY;
-    }
-
-    calcSection(el) {
-        if (
-            window.scrollY + this.browserHeight > el.offsetTop &&
-            window.scrollY < el.offsetTop + el.offsetHeight
-        ) {
-            let scrollPercent =
-                (el.getBoundingClientRect().top / this.browserHeight) * 100;
-            if (
-                (scrollPercent < 25 &&
-                    scrollPercent > -0.1 &&
-                    this.scrollDirection === "down") ||
-                (scrollPercent < 33 && this.scrollDirection === "up")
-            ) {
-                let matchingLink = el.getAttribute("data-matching-link");
-                document
-                    .querySelectorAll(`.primary-nav a:not(${matchingLink})`)
-                    .forEach((el) =>
+                    this.navLinks.forEach((el) =>
                         el.classList.remove("primary-nav--is-current-link")
                     );
-                document
-                    .querySelector(matchingLink)
-                    .classList.add("primary-nav--is-current-link");
-            }
-        }
+
+                    link.classList.add("primary-nav--is-current-link");
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(highlightLink, {
+            root: null,
+            threshold: 0.5,
+        });
+
+        this.pageSections.forEach((section) => observer.observe(section));
     }
 }
 
